824f6349751b9ba0ba35a713b8624f58fdd17823
Andrey <Radical21@yandex.ru>
2024-07-12 00:07:02 +0300

    ver 0.2

--- web/main/views.py
+++ web/main/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import context
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'магазин'
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About')
